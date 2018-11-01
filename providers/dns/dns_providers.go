156670291ab2ff685bccb87d2f008ee2d7f8cff6
Fernandez Ludovic <lfernandez.dev@gmail.com>
2018-11-01 23:37:00 +0100

    refactor: PreCheckDNS become an option.

--- providers/dns/dns_providers.go
+++ providers/dns/dns_providers.go
@@ -54,6 +54,7 @@ import (
 	"github.com/xenolf/lego/providers/dns/vultr"
 )
 
+// FIXME rename
 // NewDNSChallengeProviderByName Factory for DNS providers
 func NewDNSChallengeProviderByName(name string) (acme.ChallengeProvider, error) {
 	switch name {
